44216d7af491744f31ec399571d7520802a528d6
Jeremy GROSJEAN <jeremy.grosjean.icam@gmail.com>
2021-10-05 15:22:49 +0200

    mis a jour fonction async

--- index3.js
+++ index3.js
@@ -1,159 +1,78 @@
-const bodyElements = document.getElementsByTagName("body");
+function ajouterLigne(note) {
 
-if (bodyElements.length > 0) { //on verifie que body a un element (=existe)
-    const body = document.getElementsByTagName("body")[0]; //on crée la variable body et on lui associe le body du html
-    const newUl = document.createElement("ul"); //on crée la variable et l'element ul
-    newUl.id = "liste";
+    const newLi = document.createElement("li");
+    newLi.innerText = note.content;
+    newLi.id = 'li'+note.id;
 
-    const newInput = document.createElement("input");
-    newInput.id = "input"
-    body.appendChild(newInput);
-
-    const newButton = document.createElement("button");
-    body.appendChild(newButton);
-    newButton.innerText = "Ajouter un élément"
-    newButton.id = "button";
-
-    // const newSupprButton = document.createElement("button");
-    // body.appendChild(newSupprButton);
-    // newSupprButton.innerText = "Supprimer le dernier éléments"
-    // newSupprButton.id = "suppr";
-
-    fetch("http://localhost:3000/notes", {
-            method: 'GET'
+    const buttonDelete = document.createElement("button");
+    buttonDelete.innerText = "Supprimer";
+    buttonDelete.id = note.id;
+    
+    buttonDelete.addEventListener('click', function(event) {
+        fetch(`http://localhost:3000/notes/${event.target.id}`, {
+            method: 'DELETE'
         })
         .then(response => {
             return response.json()
         })
-        .then(notes => {
+        .then(note => {
+            document.querySelector(`li#li${event.target.id}`).remove()
+        });
+    })
+    newLi.appendChild(buttonDelete);
+    return newLi;
+}
 
-            body.appendChild(newUl); //on associe la balise ul en tant qu'enfant du la balsie body
+async function ajouterNote() {
+    const newUl = document.getElementById('myUl');
+    const raw = JSON.stringify({
+        "content": document.getElementById('note').value
+        });
 
-            console.log(notes);
+    const myHeaders = new Headers();
+    myHeaders.append("Content-Type", "application/json");
 
-            for (let index = 0; index < notes.length; index++) {
+    const newNote = await fetch("http://localhost:3000/notes", {
+        method: 'POST',
+        headers: myHeaders,
+        body: raw
+    }).then(response => {
+        return response.json()
+    });
 
-                const newLi = document.createElement("li"); //on crée la variable et l'element li
-                newUl.appendChild(newLi); //on associe la balise li en tant qu'enfant du la balsie ul
-                newLi.textContent = notes[index].content;
-                newLi.id = "index " + notes[index].id;
+    const newLi = ajouterLigne(newNote);
+    newUl.appendChild(newLi);
 
+    document.getElementById('note').value = '';
+}
 
-                const buttonSuppr = document.createElement("button");
-                newLi.appendChild(buttonSuppr);
-                buttonSuppr.innerText = "Supprimer l'élément";
-                buttonSuppr.id = notes[index].id;
-
-                buttonSuppr.addEventListener("click", function (e) {
-
-                    newLi.remove(e.id);
-
-                    fetch("http://localhost:3000/notes/" + buttonSuppr.id, {
-                            method: 'DELETE',
-                            headers: {
-                                "Content-type": "application/json"
-                            },
-
-                        })
-                        .then(response => response.text())
-                        .then(result => console.log(result))
-                        .catch(error => console.log('error', error));
-
-
-
-                });
-
-
-            }
+async function chargerNotes() {
+    const bodyElements = document.getElementsByTagName('body');
+    if (bodyElements.length > 0) {
+        const body = bodyElements[0];
+        const newUl = document.createElement("ul");
+        newUl.id = 'myUl';
+    
+        const notes = await fetch("http://localhost:3000/notes", {
+            method: 'GET'
         })
-        .catch(error => console.log('error', error));
+        .then(response => {
+            return response.json()
+        });
 
-
-
-    document.getElementById("button").addEventListener("click", function () {
-
-        if (newInput.value != '') { //on verifie que l'input n'est pas vide
-            const newLi = document.createElement("li"); //on crée la variable et l'element li
-            newUl.appendChild(newLi); //on associe la balise li a la balise ul
-            newLi.textContent = newInput.value; //on affecte la valeur de l'input au contenu du li
-
-
-
-
-            let myHeaders = new Headers();
-            myHeaders.append("Content-Type", "application/json");
-
-            let raw = JSON.stringify({
-                "content": newInput.value
-            })
-
-            fetch("http://localhost:3000/notes", {
-                    method: 'POST',
-                    headers: myHeaders,
-                    body: raw,
-                    redirect: "follow"
-
-                })
-                .then(response => {
-                    return response.json()
-                })
-                .then(result => {
-
-                    const buttonSuppr = document.createElement("button");
-                    newLi.appendChild(buttonSuppr);
-                    buttonSuppr.innerText = "Supprimer l'élément";
-                    buttonSuppr.id = result.id;
-                    newLi.id = "index" + result.id;
-
-                    buttonSuppr.addEventListener("click", function (e) {
-
-                        newLi.remove(e.id);
-    
-                        fetch("http://localhost:3000/notes/" + buttonSuppr.id, {
-                                method: 'DELETE',
-                                headers: {
-                                    "Content-type": "application/json"
-                                },
-    
-                            })
-                            .then(response => response.text())
-                            .then(result => console.log(result))
-                            .catch(error => console.log('error', error));
-    
-    
-    
-                    });
-
-
-                })
-                .catch(error => console.log('error', error));
-
-
-            newInput.value = ''; //on reset l'input
+        for (let index = 0 ; index < notes.length; index ++) {
+            const note = notes[index];
+            const newLi = ajouterLigne(note);
+            newUl.appendChild(newLi);
         }
 
-    })
+        body.appendChild(newUl);
 
-
-
-
+        document.getElementById('btn1').addEventListener('click', ajouterNote);
+    }
 }
 
+const result = chargerNotes().then()
+.catch(error => console.log('error', error));
 
 
-
-
-//     });
-// }
-
-// console.log(document.getElementsByTagName("li"));
-
-
-
-function addLine(){
-
-}
-
-function loadNotes(){
-
-}
